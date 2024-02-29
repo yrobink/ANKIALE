1b7244480a778a3d21e6ba3cc08ad96a9fc3a624
yrobin <yoann.robin.k@gmail.com>
2024-02-29 17:37:51 +0100

    Add a attributed fintensity method

--- BSAC/cmd/__cmd_attribute.py
+++ BSAC/cmd/__cmd_attribute.py
@@ -605,23 +605,291 @@ def run_bsac_cmd_attribute_fcreturnt(arg):
 ##}}}
 
 
+def _attribute_fintensity_parallel( *args , ovars , nslaw_class , side , mode , ci ):##{{{
+	
+	## Extract
+	pars    = { ovar : arg for ovar,arg in zip(ovars,args) } ## Parameters
+	bias    = args[-2]
+	xIF     = args[-1]
+	nslaw   = nslaw_class()
+	kwargsF = { p : pars[p+"F"] for p in nslaw.coef_kind }
+	kwargsC = { p : pars[p+"C"] for p in nslaw.coef_kind }
+	
+	## Set output
+	pF  = np.zeros_like( pars[ovars[0]] ) + np.nan
+	pC  = np.zeros_like( pars[ovars[0]] ) + np.nan
+	IF  = np.zeros_like( pars[ovars[0]] ) + xIF
+	IC  = np.zeros_like( pars[ovars[0]] ) + np.nan
+	
+	## Attribution
+	pF = nslaw.cdf_sf(  IF , side = side , **kwargsF )
+	pC = nslaw.cdf_sf(  IF , side = side , **kwargsC )
+	IC = nslaw.icdf_sf( pF , side = side , **kwargsC ) + bias
+	IF = IF + bias
+	
+	## Others variables
+	with warnings.catch_warnings():
+		warnings.simplefilter("ignore")
+		RF  = 1. / pF
+		RC  = 1. / pC
+		dI  = IF - IC
+		PR  = pF / pC
+	
+	## Compute CI
+	if mode == "quantile":
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore")
+			pF = np.quantile( pF , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			pC = np.quantile( pC , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			RF = np.quantile( RF , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			RC = np.quantile( RC , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			IF = np.quantile( IF , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			IC = np.quantile( IC , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			dI = np.quantile( dI , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+			PR = np.quantile( PR , [ci/2,0.5,1-ci/2] , axis = 0 , method = "median_unbiased" )
+	
+	return tuple([pF,pC,RF,RC,IF,IC,dI,PR])
+##}}}
+
+## run_bsac_cmd_attribute_fintensity ##{{{
+@log_start_end(logger)
+def run_bsac_cmd_attribute_fintensity(arg):
+	
+	## Parameters
+	clim      = bsacParams.clim
+	time      = clim.time
+	n_samples = bsacParams.n_samples
+	tmp       = bsacParams.tmp
+	side      = bsacParams.config.get("side","right")
+	mode      = bsacParams.config.get("mode","sample")
+	ci        = bsacParams.config.get("ci",0.05)
+	nslaw     = clim._nslaw_class()
+	sp_dims   = clim.d_spatial
+	
+	## Logs
+	logger.info(  " * Configuration" )
+	logger.info( f"   => mode: {mode}" )
+	logger.info( f"   => side: {side}" )
+	logger.info( f"   => ci  : {ci}" )
+	
+	## Draw parameters for the attribution
+	logger.info( " * Draw parameters" )
+	zdraw   = rvs_climatology( clim , n_samples , tmp = bsacParams.tmp , n_jobs = bsacParams.n_jobs , mem_limit = bsacParams.total_memory )
+	ovars   = [key for key in zdraw if key not in ["XF","XC","XA","hpar"]]
+	periods = np.array(zdraw["XF"].coords[zdraw["XF"].dims.index("period")])
+	if mode == "sample":
+		modes = np.array(zdraw[ovars[0]].coords[zdraw[ovars[0]].dims.index("sample")])
+	elif mode == "quantile":
+		modes = np.array(["QL","BE","QU"])
+	else:
+		raise ValueError( f"Invalid mode ({mode})" )
+	n_modes = modes.size
+	
+	## Read the intensity
+	name,ifile = bsacParams.input[0].split(",")
+	xIF = xr.open_dataset(ifile)[name]
+	
+	## Check the spatial dimensions
+	for d in clim._spatial:
+		if d not in xIF.dims:
+			raise Exception( f"Spatial dimension missing: {d}" )
+		if not xIF[d].size == clim._spatial[d].size:
+			raise Exception( f"Bad size of the dimension {d}: {xIF[d].size} != {clim._spatial[d].size}" )
+	
+	## Reorganize dimension
+	sdims   = list(clim._spatial)
+	cdims   = [ d for d in xIF.dims if d not in sdims ]
+	ccoords = [xIF[d] for d in cdims]
+	cshape  = [xIF[d].size for d in cdims]
+	dims    = tuple(cdims + sdims)
+	xIF     = xIF.transpose(*dims)
+	
+	for i,d in enumerate(cdims):
+		if d in [mode] + list(zdraw[ovars[0]].dims)[1:]:
+			xIF = xIF.rename( { d : "BSAC_" + d } )
+			cdims[i] = "BSAC_" + d
+	
+	## Remove bias
+	oB  = clim.bias[list(clim.bias)[-1]]
+	xIF = xIF - oB
+	
+	## Output
+	dims   = cdims   + [mode   ] + list(zdraw[ovars[0]].dims)[1:]
+	coords = ccoords + [modes  ] + list(zdraw[ovars[0]].coords)[1:]
+	shape  = cshape  + [n_modes] + list(zdraw[ovars[0]].shape)[1:]
+	out = {
+	    "pF" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "pF" ) ) ),
+	    "pC" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "pC" ) ) ),
+	    "RF" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "RF" ) ) ),
+	    "RC" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "RC" ) ) ),
+	    "IF" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "IF" ) ) ),
+	    "IC" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "IC" ) ) ),
+	    "dI" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "dI" ) ) ),
+	    "PR" : XZarr.from_value( np.nan , shape = shape , dims = dims , coords = coords , zfile = random_zfile( os.path.join( tmp , "PR" ) ) )
+	}
+	
+	## Select the good function
+	if arg == "fintensity":
+		_attribute_fcintensity_parallel = _attribute_fintensity_parallel
+	
+	## Dask parameters
+	dask_gufunc_kwargs = {}
+	dask_gufunc_kwargs["output_sizes"] = { **{} , **{ mode : modes.size } }
+	
+	## Loop on spatial variables
+	block = max( 0 , int( np.power( bsacParams.n_jobs , 1. / len(clim.s_spatial) ) ) ) + 1
+	logger.info( " * Loop on spatial variables" )
+	for idx in itt.product(*[range(0,s,block) for s in clim.s_spatial]):
+		
+		##
+		s_idx = tuple([slice(s,s+block,1) for s in idx])
+		f_idx = tuple( [slice(None) for _ in range(3)] ) + s_idx
+		I_idx = tuple( [slice(None) for _ in range(len(cdims))] ) + s_idx
+		
+		##
+		draw = { ovar : zdraw[ovar].get_orthogonal_selection(f_idx).chunk( { d : 1 for d in sp_dims } )  for ovar in ovars }
+		bias = clim.bias[clim.names[-1]][s_idx]
+		IF   = xIF[I_idx]
+		
+		#
+		res = xr.apply_ufunc( _attribute_fcintensity_parallel , *[draw[ovar] for ovar in ovars] , bias , IF ,
+		                    input_core_dims    = [[ "sample","time","period"] for _ in range(len(draw))] + [[]] + [[]],
+		                    output_core_dims   = [[mode,"time","period"] for _ in range(8)],
+		                    output_dtypes      = [draw[ovars[0]].dtype for _ in range(8)],
+		                    vectorize          = True,
+		                    dask               = "parallelized",
+		                    kwargs             = { "ovars" : ovars , "nslaw_class" : clim._nslaw_class , "side" : side , "mode" : mode , "ci" : ci },
+		                    dask_gufunc_kwargs = dask_gufunc_kwargs
+		                    )
+		
+		res = [ r.transpose( *out["pF"].dims ).compute() for r in res ]
+		for key,r in zip(out,res):
+			out[key].set_orthogonal_selection( tuple( [slice(None) for _ in range(len(cdims)+3)] ) + s_idx , r.values )
+	
+	## And save
+	logger.info( " * Save in netcdf" )
+	ofile = bsacParams.output
+	with netCDF4.Dataset( ofile , "w" ) as ncf:
+		
+		## Define dimensions
+		ncdims = {
+		           mode : ncf.createDimension(     mode      , n_modes ),
+		       "period" : ncf.createDimension( "period"      , len(clim.dpers) ),
+		       "time"   : ncf.createDimension(   "time" ),
+		}
+		for d,s in zip(cdims,cshape):
+			ncdims[d] = ncf.createDimension( d , s )
+		
+		spatial = ()
+		if clim._spatial is not None:
+			for d in clim._spatial:
+				ncdims[d] = ncf.createDimension( d , clim._spatial[d].size )
+			spatial = tuple([d for d in clim._spatial])
+		
+		## Define variables
+		ncvars = {
+		           mode : ncf.createVariable(     mode      , str       , (mode,)     ),
+		       "period" : ncf.createVariable( "period"      , str       , ("period",) ),
+		       "time"   : ncf.createVariable( "time"        , "float32" , ("time"  ,) )
+		}
+		ncvars[mode    ][:]      = modes
+		ncvars["period"][:]      = periods
+		if clim._spatial is not None:
+			for d in clim._spatial:
+				ncvars[d] = ncf.createVariable( d , "double" , (d,) )
+				ncvars[d][:] = np.array(clim._spatial[d]).ravel()
+		for d in cdims:
+			if isinstance(xIF[d][0].values,tuple([dt.datetime,np.datetime64,cftime.datetime])):
+				ncvars[d] = ncf.createVariable( d , "float32" , (d,) )
+			else:
+				ncvars[d] = ncf.createVariable( d , xIF[d].values.dtype , (d,) )
+				ncvars[d][:] = xIF[d].values
+		
+		if mode == "quantile":
+			ncvars[mode].setncattr( "confidence_level" , ci )
+			ncvars["quantile_level"] = ncf.createVariable( "quantile_levels" , "float32" , ("quantile") )
+			ncvars["quantile_level"][:] = np.array([ci/2,0.5,1-ci/2])
+		
+		## Fill time axis
+		calendar = "standard"
+		units    = "days since 1750-01-01 00:00"
+		ncvars["time"][:]  = cftime.date2num( [cftime.DatetimeGregorian( int(y) , 1 , 1 ) for y in time] , units = units , calendar = calendar )
+		ncvars["time"].setncattr( "standard_name" , "time"      )
+		ncvars["time"].setncattr( "long_name"     , "time_axis" )
+		ncvars["time"].setncattr( "units"         , units       )
+		ncvars["time"].setncattr( "calendar"      , calendar    )
+		ncvars["time"].setncattr( "axis"          , "T"         )
+		
+		## Variables
+		for key in out:
+			ncvars[key] = ncf.createVariable( key , "float32" , tuple(cdims) + (mode,"time","period") + spatial , compression = "zlib" , complevel = 5 , chunksizes = tuple([1 for _ in range(len(cdims))]) + (1,1,1) + clim.s_spatial )
+		
+		## Attributes
+		ncvars["pF"].setncattr( "description" , "Probability in the Factual world" )
+		ncvars["pC"].setncattr( "description" , "Probability in the Counter factual world" )
+		ncvars["RF"].setncattr( "description" , "Return time in the Factual world" )
+		ncvars["RC"].setncattr( "description" , "Return time in the Counter factual world" )
+		ncvars["IF"].setncattr( "description" , "Intensity in the Factual world" )
+		ncvars["IC"].setncattr( "description" , "Intensity in the Counter factual world" )
+		ncvars["PR"].setncattr( "description" , "Change in probability between Factual and Counter factual world" )
+		ncvars["dI"].setncattr( "description" , "Change in intensity between Factual and Counter factual world" )
+		
+		## Find the blocks to write netcdf
+		blocks  = [1 for _ in range(len(cdims))] + [1,1,1]
+		sizes   = list(cshape) + [n_modes,time.size,len(clim.dpers)]
+		nsizes  = list(cshape) + [n_modes,time.size,len(clim.dpers)]
+		sp_mem  = SizeOf( n = int(np.prod(clim.s_spatial) * np.finfo('float32').bits // SizeOf(n = 0).bits_per_octet) , unit = "o" )
+		tot_mem = SizeOf( n = int(min( 0.8 , 3 * bsacParams.frac_memory_per_array ) * bsacParams.total_memory.o) , unit = "o" )
+		nfind   = [True for _ in range(len(cdims))] + [True,True,True]
+		while any(nfind):
+			i = np.argmin(nsizes)
+			blocks[i] = sizes[i]
+			while int(np.prod(blocks)) * sp_mem > tot_mem:
+				if blocks[i] < 2:
+					blocks[i] = 1
+					break
+				blocks[i] = blocks[i] // 2
+			nfind[i]  = False
+			nsizes[i] = np.inf
+		logger.info( f"   => Blocks size {blocks}" )
+		
+		## Fill
+		idx_sp = tuple([slice(None) for _ in range(len(clim._spatial))])
+		for idx in itt.product(*[range(0,s,block) for s,block in zip(sizes,blocks)]):
+			
+			s_idx = tuple([slice(s,s+block,1) for s,block in zip(idx,blocks)])
+			idxs = s_idx + idx_sp
+			
+			for key in out:
+				xdata = out[key].get_orthogonal_selection(idxs)
+				ncvars[key][idxs] = xdata.values
+		
+		## Global attributes
+		ncf.setncattr( "creation_date" , str(dt.datetime.utcnow())[:19] + " (UTC)" )
+		ncf.setncattr( "BSAC_version"  , version )
+		ncf.setncattr( "description" , f"Attribute {arg}" )
+##}}}
+
+
 ## run_bsac_cmd_attribute ##{{{
 @log_start_end(logger)
 def run_bsac_cmd_attribute():
 	
-	avail_arg = ["event","freturnt","creturnt"]
+	avail_arg = ["event","freturnt","creturnt","fintensity"]
 	try:
 		arg = bsacParams.arg[0]
 	except:
-		raise ValueError( "A argument must be given for the attribute command ({', '.join(avail_arg)})" )
+		raise ValueError( f"A argument must be given for the attribute command ({', '.join(avail_arg)})" )
 	
 	if not arg in avail_arg:
-		raise ValueError( "Bad argument for the attribute command ({', '.join(avail_arg)})" )
+		raise ValueError( f"Bad argument for the attribute command ({', '.join(avail_arg)})" )
 	
 	if arg == "event":
 		run_bsac_cmd_attribute_event()
 	if arg in ["freturnt","creturnt"]:
 		run_bsac_cmd_attribute_fcreturnt(arg)
+	if arg in ["fintensity"]:
+		run_bsac_cmd_attribute_fintensity(arg)
 	
 ##}}}
 
